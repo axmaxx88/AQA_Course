2265bd8ac4ee2e5d679c9fcd9a83db6b97238ead
axmaxx88 <92793703+axmaxx88@users.noreply.github.com>
2022-06-12 08:51:06 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,9 +4,9 @@ import time
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_basket_button(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     time.sleep(30)
-    basket_button = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form .btn")
-    assert basket_button, "The page doesn't have an 'Add to basket button'"
+    cart_button = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form .btn")
+    assert cart_button, "The page doesn't have an 'Add to cart button'"
 
